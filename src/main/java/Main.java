b2017992b5ca42f391eda59d597c23ce9159bcf6
dilly <matthew.j.dillard@gmail.com>
2021-01-27 22:11:27 -0500

    Title and OfferingName pairs no longer repeat, formatting is correct, empty response case handled

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,12 @@
 import kong.unirest.GenericType;
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public class Course {
         String Title;
         String OfferingName;
-
-        @Override
-        public String toString() {
-            return OfferingName + " " + Title + "\n";
-        }
     }
 
     public static void main(String[] args) {
@@ -31,13 +23,25 @@ public class Main {
 
     private static void search(String query) {
         final String BASE_URL = "https://sis.jhu.edu/api/classes?key=";
-        final String ADVANCED = "&School=Whiting%20School%20of%20Engineering&Department=EN%20Computer%20Science&CourseTitle={query}";
+        final String ADVANCED = "&School=Whiting%20School%20of%20Engineering&Department=EN%20Computer%20Science&Term=Spring%202021&CourseTitle={query}";
         final String KEY = System.getenv("JHU_API_KEY");
 
         String endpoint = BASE_URL + KEY + ADVANCED;
 
-        List<Course> courses = Unirest.get(endpoint).routeParam("query", query).asObject(new GenericType<List<Course>>(){}).getBody();
+        List<Course> coursesList = Unirest.get(endpoint).routeParam("query", query).asObject(new GenericType<List<Course>>(){}).getBody();
 
-        System.out.println(courses);
+        HashMap<String, String> courses = new HashMap<>();
+
+        for (Course course : coursesList) {
+            courses.put(course.OfferingName, course.Title);
+        }
+
+        if (courses.size() == 0) {
+            System.out.println("No course title contains the entered search query!");
+        } else {
+            for (Map.Entry i : courses.entrySet()) {
+                System.out.println(i.getKey() + " " + i.getValue());
+            }
+        }
     }
 }
